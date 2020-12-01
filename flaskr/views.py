e0097881be8ce0285ebafdfb67a62b3908834337
Satoshi Ota <sota.developer@gmail.com>
2020-12-01 12:46:44 +0900

    first commit

---
+++ flaskr/views.py
@@ -0,0 +1,22 @@
+from flask import flash, redirect, render_template, request, url_for
+
+from flaskr import app, db
+from flaskr.models import Entry
+
+
+@app.route('/')
+def show_entries():
+    entries = Entry.query.order_by(Entry.id.desc()).all()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    entry = Entry(
+        title=request.form['title'],
+        text=request.form['text']
+    )
+    db.session.add(entry)
+    db.session.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
